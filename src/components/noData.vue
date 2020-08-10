52da85fb7b0a0337a63fff8c6eda9d5838142392
卢方舟 <lufangzhou@adinnet.cn>
2020-08-10 10:53:12 +0800

    feat: experts config page

---
+++ src/components/noData.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="p-no-data">
+    <h2>{{info}}</h2>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['info'],
+    name: '',
+    components: {},
+    data() {
+      return {}
+    },
+    created() {
+    },
+    mounted() {
+    },
+    methods: {}
+  }
+</script>
+<style lang="scss" scoped>
+  .p-no-data {
+    height: 200px;
+    @include vertical-center;
+  }
+</style>
--- src/views/experts/modules/entryConfig.vue
+++ src/views/experts/modules/entryConfig.vue
@@ -17,7 +17,10 @@
         <li>信息内容</li>
         <li>操作</li>
       </ul>
-      <ul class="p-table-row p-body-row"
+      <no-data v-if="isNoData" info="暂无配置数据信息！"></no-data>
+      <ul
+          v-if="!isNoData"
+          class="p-table-row p-body-row"
         v-for="(i, index) in configList"
           :key="index"
       >
@@ -53,17 +56,39 @@
         :visible.sync="dialog.isShow"
         width="30%"
     >
-      <div class="dialog-panel">
+      <div class="entry-config-dialog">
         <el-form label-width="100px">
           <el-form-item label="专家信息项:">
             <el-input v-model="dialog.configLabel"></el-input>
           </el-form-item>
           <el-form-item label="信息内容:">
-            <el-tag v-for="(item, index2) in dialog.children"
-                    :key="index2"
-            >
-              {{item}}
-            </el-tag>
+            <div class="info-tag">
+              <el-tag v-for="(item, index2) in dialog.children"
+                      :key="index2"
+                      closable
+                      :disable-transitions="false"
+                      size="medium"
+                      @close="tagRemove(item)"
+              >
+                {{item}}
+              </el-tag>
+
+              <el-input
+                  class="input-new-tag"
+                  v-if="tagInput.inputVisible"
+                  v-model.trim="tagInput.inputValue"
+                  ref="saveTagInput"
+                  size="small"
+                  @keyup.enter.native="addTag"
+                  @blur="addTag"
+              >
+              </el-input>
+              <el-button
+                  v-else
+                  class="button-new-tag"
+                  size="small"
+                  @click="showTagInput">+ 新增</el-button>
+            </div>
           </el-form-item>
         </el-form>
       </div>
@@ -78,24 +103,33 @@
 </template>
 <script>
   import panelHead from "@c/panelHead.vue"
+  import noData from "@c/noData.vue"
   export default {
     name: '',
     components: {
-      panelHead
+      panelHead,
+      noData
     },
     data() {
       return {
+        isNoData: false,
+        isLoading: false,
+
         configList: [
           {
             label: '单位性质',
-            children: ['基地', '事业单位', '政府单位','基地', '事业单位', '政府单位']
+            children: ['基地', '事业单位', '政府单位','基地1', '事业单位1', '政府单位1']
           }
         ],
         dialog: {
-          isShow: true,
+          isShow: false,
           cate: 'edit', //edit add
           configLabel: '',
           children: []
+        },
+        tagInput: {
+          inputVisible: false,
+          inputValue: ''
         }
       }
     },
@@ -109,7 +143,7 @@
         this.dialog.cate = cate
         if(cate === 'edit'){
           this.dialog.configLabel = info.label
-          this.dialog.children = info.children
+          this.dialog.children = [...info.children]
         }else{
           this.dialog.configLabel = ''
           this.dialog.children = []
@@ -117,10 +151,70 @@
       },
       delInfo(i) {
         console.log(76, i)
+        this.configList.splice(
+          this.configList.findIndex(c=>i.label===c.label)
+          ,1)
       },
       /**********************dialog***********************/
       save() {
-        this.dialog.isShow = false
+        let msg = '', flag = true
+        if(this.dialog.children.length === 0) {
+          flag = false
+          msg = '信息内容不能为空！'
+        }else if(this.dialog.configLabel === '') {
+          flag = false
+          msg = '专家信息不能为空！'
+        }
+        if(flag){
+          this.dialog.isShow = false
+          //响应请求
+          this.setConfig()
+        }else {
+          this.$message({type:'error', message: msg})
+        }
+      },
+      tagRemove(item) {
+        const removeIndex = this.dialog.children.findIndex(c => item === c)
+        this.dialog.children.splice(removeIndex, 1)
+      },
+      addTag() {
+        let inputValue = this.tagInput.inputValue
+        //不能重复添加
+        const findIndex = this.dialog.children.findIndex(c=>c===inputValue)
+        if(findIndex !== -1){
+          this.$message({type:"error", message: '不能重复添加！'})
+          return false
+        }
+        if(inputValue){
+          this.dialog.children.push(inputValue)
+        }
+        this.tagInput.inputVisible = false
+        this.tagInput.inputValue = ''
+      },
+      showTagInput() {
+        this.tagInput.inputVisible = true
+        this.$nextTick(_ => {
+          this.$refs.saveTagInput.$refs.input.focus();
+        })
+      },
+      /**********************request***********************/
+      setConfig() {
+        if(this.dialog.cate === 'add'){
+          this.configList.push({
+            label: this.dialog.configLabel,
+            children: [...this.dialog.children]
+          })
+        }else if(this.dialog.cate === 'edit'){
+
+        }
+        console.log(199, this.configList)
+      },
+    },
+    watch: {
+      configList(newVal, oldVal){
+        newVal.length===0
+          ?this.isNoData = true
+          :this.isNoData = false
       }
     }
   }
@@ -173,6 +267,37 @@
             margin-left: 20px;
           }
         }
+      }
+    }
+  }
+</style>
+
+<style lang="scss">
+  .p-entry-config{
+    .entry-config-dialog{
+      .info-tag{
+        display: flex;
+        flex-wrap: wrap;
+        .el-tag{
+          margin-right: 20px;
+          margin-top: 10px;
+        }
+      }
+      .button-new-tag{
+        background-color: #ebf5ff;
+        border-color: #d6ebff;
+        color: #3399ff;
+        display: inline-block;
+        padding: 0 10px;
+        font-size: 12px;
+        border-width: 1px;
+        border-style: solid;
+        border-radius: 4px;
+        box-sizing: border-box;
+        white-space: nowrap;
+        margin-top: 10px;
+        height: 28px;
+        line-height: 26px;
       }
     }
   }
